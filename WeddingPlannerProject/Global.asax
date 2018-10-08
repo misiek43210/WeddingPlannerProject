61145268a837c7c5eaf04a7571fd216774664c93
Michał Mamełka <michalmamelka@outlook.com>
2018-10-08 20:00:14 +0200

    Dodaj pliki projektów.

---
+++ WeddingPlannerProject/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WeddingPlannerProject.MvcApplication" Language="C#" %>
---
+++ WeddingPlannerProject/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WeddingPlannerProject.Startup))]
+namespace WeddingPlannerProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
